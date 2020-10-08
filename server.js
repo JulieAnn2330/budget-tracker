4ed87c87a76ad870cf5fa379c5320b763e8ead61
Julie Schaub <julie.schaub@hotmail.com>
2020-10-07 22:16:12 -0500

    added db.js

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
+/* Pseudocode:
+1. Add dependencies
+  a. npm i -- DONE
+  b. npm i express -- DONE
+  c. npm i morgan -- DONE
+  d. npm i mongoose -- DONE
+  e. npm i compression -- DONE
+  f. npm i nodemon - so I don't have to keep restarting server -- DONE
+2. User needs
+  a. Enter deposits offline
+  b. Enter expenses offline
+  c. Populate database when brought online
+3. Files to create/update
+  a. db.js in public - for indexedDB
+  b. Update server for heroku
+  c. Update package.json for nodeomon -- DONE
+4. Deployment
+  a. MongoDB Atlas
+  b. Heroku
+5. Submission criteria
+  a. URL to deployed app on heroku
+  b. GitHub repo
+*/
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
